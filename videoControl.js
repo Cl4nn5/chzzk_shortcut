b4a778c3d9da54e8ada5e5543767d8d42b0c77b5
Clann_Home:김홍제_집 <restoreclann@gmail.com>
2024-01-04 20:41:04 +0900

    시작시 비디오에 포커스를 제공합니다
    업데이트 내역 수정

--- videoControl.js
+++ videoControl.js
@@ -8,41 +8,57 @@
 // pzp-button pzp-pc-viewmode-button pzp-pc__viewmode-button pzp-pc-ui-button pzp-pc-ui-button--clicked
 
 console.log("videoControl.js is loaded _ by Clann");
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 // 이벤트 등록
-document.addEventListener("keydown", (e) => {
-  // error handling
-  if (!e.target.classList.contains("pzp")) {
-    // console.error("pzp가 아닙니다.");
-    return;
-  }
 
-  // 1. play/pause 버튼을 클릭함
-  if (e.key === "k" || e.key === "K") {
-    const playToggleBtn = document.querySelector(".pzp-pc-playback-switch");
-    playToggleBtn.click();
-    return;
-  }
+(function () {
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  // 2. mute/unmute 버튼을 클릭함
-  if (e.key === "m" || e.key === "M") {
-    const muteToggleBtn = document.querySelector(".pzp-pc-volume-button");
-    muteToggleBtn.click();
-    return;
-  }
+  // 1. 단축키
+  const createShortcut = () => {
+    document.addEventListener("keydown", (e) => {
+      // error handling
+      if (!e.target.classList.contains("pzp")) {
+        // console.error("pzp가 아닙니다.");
+        return;
+      }
 
-  // 3. toggle watching mode 버튼을 클릭함
-  if (e.key === "t" || e.key === "T") {
-    const viewModeToggleBtn = document.querySelector(".pzp-pc-viewmode-button");
-    viewModeToggleBtn.click();
-    return;
-  }
+      // 1. play/pause 버튼을 클릭함
+      if (e.key === "k" || e.key === "K") {
+        const playToggleBtn = document.querySelector(".pzp-pc-playback-switch");
+        playToggleBtn.click();
+        return;
+      }
 
-  // 4. full screen 버튼을 클릭함
-  if (e.key === "f" || e.key === "F") {
-    const fullScreenBtn = document.querySelector(".pzp-pc-fullscreen-button");
-    fullScreenBtn.click();
-    return;
-  }
-});
+      // 2. mute/unmute 버튼을 클릭함
+      if (e.key === "m" || e.key === "M") {
+        const muteToggleBtn = document.querySelector(".pzp-pc-volume-button");
+        muteToggleBtn.click();
+        return;
+      }
+
+      // 3. toggle watching mode 버튼을 클릭함
+      if (e.key === "t" || e.key === "T") {
+        const viewModeToggleBtn = document.querySelector(".pzp-pc-viewmode-button");
+        viewModeToggleBtn.click();
+        return;
+      }
+
+      // 4. full screen 버튼을 클릭함
+      if (e.key === "f" || e.key === "F") {
+        const fullScreenBtn = document.querySelector(".pzp-pc-fullscreen-button");
+        fullScreenBtn.click();
+        return;
+      }
+    });
+  };
+  // 2. 포커스
+  const focusVideo = () => {
+    const video = document.querySelector(".pzp-pc");
+    video.focus();
+  };
+
+  // 실행단
+  createShortcut();
+  delay(1000).then(() => focusVideo());
+})();
